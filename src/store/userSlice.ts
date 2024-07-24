454185898df4dd8467cf70483cca392c97bc5ec9
Abdulloh <email@example.com>
2024-07-24 10:51:28 +0500

    .

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,8 +1,43 @@
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { User } from "../types";
+
+// export interface UserState {
+//   user: User | null;
+//   authChange: boolean;
+// }
+
+// const initialState: UserState = {
+//   user: JSON.parse(localStorage.getItem("user") || "null"),
+//   authChange: false,
+// };
+
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     login: (state, action: PayloadAction<User>) => {
+//       state.user = action.payload;
+//       localStorage.setItem("user", JSON.stringify(action.payload));
+//       state.authChange = true;
+//     },
+//     logout: (state) => {
+//       state.user = null;
+//       localStorage.removeItem("user");
+//       state.authChange = false;
+//     },
+//     authChange: (state, action: PayloadAction<boolean>) => {
+//       state.authChange = action.payload;
+//     },
+//   },
+// });
+
+// export const { login, logout, authChange } = userSlice.actions;
+// export default userSlice.reducer;
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { User } from "../types";
+import { LoginUser } from "../types";
 
 export interface UserState {
-  user: User | null;
+  user: LoginUser | null;
   authChange: boolean;
 }
 
@@ -15,7 +50,7 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<User>) => {
+    login: (state, action: PayloadAction<LoginUser>) => {
       state.user = action.payload;
       localStorage.setItem("user", JSON.stringify(action.payload));
       state.authChange = true;
